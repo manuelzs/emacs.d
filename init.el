12425cf6f1d7c57394a3b1f260ef2bbae8ad449d
Manuel Zapata <manuelzs@gmail.com>
2013-02-01 12:17:03 +0000

    FIrst comit. Very basic conf

---
+++ init.el
@@ -0,0 +1,42 @@
+(setq backup-directory-alist `(("." . "~/.emacs_backups")))
+
+(define-minor-mode silent-mode
+  "Silent mode
+Disables backup creation and auto saving."
+
+  ;; The initial value.
+  nil
+  ;; The indicator for the mode line.
+  "[Silent]"
+  ;; The minor mode bindings.
+  nil
+  
+  (if (symbol-value silent-mode)
+      (progn
+	;; disable backups
+	(set 'backup-inhibited t)
+	;; disable auto-save
+	(if auto-save-default
+	    (auto-save-mode -1)
+	  )
+	)
+    
+    ;resort to default value of backup-inhibited
+    (set 'backup-inhibited nil)
+					
+    ;resort to default auto save setting
+    (if auto-save-default
+	(auto-save-mode 1)
+      )
+    )
+
+  )
+
+(add-to-list 'load-path "~/.emacs.d/ruby-mode/")
+;; Load ruby mode when needed
+(autoload 'ruby-mode "ruby-mode" "Ruby mode" t )
+;; Assign .rb and .rake files to use ruby mode
+(setq auto-mode-alist (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
+;; Show syntax highlighting when in ruby mode
+(add-hook 'ruby-mode-hook '(lambda () (font-lock-mode 1)))
