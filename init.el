208be206ad9e10714e06d30ac9b605c47cccf55d
Manuel Zapata <manuelzs@gmail.com>
2014-05-31 09:34:17 +0000

    Added color changes to init

--- init.el
+++ init.el
@@ -75,7 +75,7 @@ Disables backup creation and auto saving."
 ;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (require 'zone)
-(zone-when-idle 60)
+(zone-when-idle 180)
 
 
 
@@ -100,3 +100,17 @@ Disables backup creation and auto saving."
 
 ;; Scroll one line - dont jump
 (setq scroll-step 1)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "Blue2"))))
+ '(minibuffer-prompt ((t (:foreground "Blue2")))))
